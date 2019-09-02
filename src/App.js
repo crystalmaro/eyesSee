21a7eda173411ddd63ec83153e4e4fee4ae7b9a8
Crystal Wang <crystalmaro@gmail.com>
2019-09-02 11:21:04 +0800

    fixing branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
+import Game from './Game';
+=======
 import Header from './Header';
 import Game from './Game';
 import Button from './Button';
 import Message from './Message';
 import Progress from './Progress';
+<<<<<<< HEAD
+<<<<<<< HEAD
+// import Firebase from './firebase';
+>>>>>>> feature/image-selection
+=======
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
+=======
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
@@ -18,3 +29,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default App;
--- src/Game.js
+++ src/Game.js
@@ -5,7 +5,20 @@ import './css/game.css';
 class Game extends Component {
   state = {
     origClass: 'imgBox',
+<<<<<<< HEAD
+<<<<<<< HEAD
+    compareClass: 'button',
+    isClicked: false,
     yesOnTop: true,
+    isCorrect: true,
+    yesImg: '../imageStock/yes.png',
+    noImg: '../imageStock/no.png',
+=======
+    yesOnTop: true,
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
+=======
+    yesOnTop: true,
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
   };
 
   clickImg = e => {
@@ -13,14 +26,87 @@ class Game extends Component {
     this.setState(preState => {
       return { origClass: preState.origClass + ' clicked' };
     });
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+    // decide which image on top
+    console.log(e.target.parentNode.id);
+
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
+    if (e.target.parentNode.id === 'no') {
+      this.setState({
+        yesOnTop: false,
+<<<<<<< HEAD
+        // check current round result
+        isCorrect: false,
+      });
+    }
+  };
+
+  clickNext = e => {
+    if (this.state.isClicked) {
+      this.setState({
+        origClass: 'imgBox',
+        isClicked: false,
+        yesOnTop: true,
+        isCorrect: true,
+      });
+    }
+  };
+
+  compareMouseDown = e => {
+    if (this.state.isClicked) {
+      if (this.state.yesOnTop) {
+        this.setState({
+          yesOnTop: false,
+          isCorrect: false,
+        });
+      }
+      if (!this.state.yesOnTop) {
+        this.setState({
+          yesOnTop: true,
+          isCorrect: true,
+        });
+      }
+      this.setState({
+        compareClass: 'button compare',
+      });
+    }
+  };
+  compareTouchDown = e => {
+    this.compareMouseDown();
+  };
+  compareMouseUp = e => {
+    if (this.state.isClicked) {
+      if (!this.state.yesOnTop) {
+        this.setState({
+          yesOnTop: true,
+          isCorrect: true,
+        });
+      }
+      if (this.state.yesOnTop) {
+        this.setState({
+          yesOnTop: false,
+          isCorrect: false,
+        });
+      }
+      this.setState({
+        compareClass: 'button',
+=======
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
+=======
     // decide which image on top
     console.log(e.target.parentNode.id);
 
     if (e.target.parentNode.id === 'no') {
       this.setState({
         yesOnTop: false,
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
       });
     }
+  };
+  compareTouchEnd = e => {
+    this.compareMouseUp();
   };
 
   render() {
@@ -33,7 +119,15 @@ class Game extends Component {
             className={this.state.origClass}
             id="yes"
           >
+<<<<<<< HEAD
+<<<<<<< HEAD
+            <img src="../imageStock/final_images/item2.png" alt="" />
+=======
             <img src="../imageStock/final_images/item4.png" alt="" />
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
+=======
+            <img src="../imageStock/final_images/item4.png" alt="" />
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
           </div>
 
           <div
@@ -42,7 +136,54 @@ class Game extends Component {
             className={this.state.origClass}
             id="no"
           >
+<<<<<<< HEAD
+<<<<<<< HEAD
+            <img src="../imageStock/final_images/item2hard2.png" alt="" />
+          </div>
+        </div>
+        <div
+          className="messageContainer"
+          style={
+            this.state.isClicked ? { display: 'flex' } : { display: 'none' }
+          }
+        >
+          <div
+            className="result"
+            style={
+              // this.state.isCorrect == true
+              this.state.isCorrect
+                ? { backgroundColor: 'green' }
+                : { backgroundColor: 'red' }
+            }
+          >
+            {/* <img
+              src={
+                this.state.isCorrect == true
+                  ? { src: '../imageStock/yes.png' }
+                  : { src: '../imageStock/no.png' }
+              }
+              /> */}
+          </div>
+          <div className="message">Icon Resolution</div>
+        </div>
+        <div className="buttonContainer">
+          <div
+            onMouseDown={this.compareMouseDown}
+            onMouseUp={this.compareMouseUp}
+            onTouchStart={this.compareTouchDown}
+            onTouchEnd={this.compareTouchEnd}
+            className={this.state.compareClass}
+          >
+            COMPARE
+          </div>
+          <div onClick={this.clickNext} className="button">
+            NEXT
+=======
             <img src="../imageStock/final_images/item4easy1.png" alt="" />
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
+=======
+            <img src="../imageStock/final_images/item4easy1.png" alt="" />
+>>>>>>> parent of 3cb8c61... finish initial image selection and compare mouse events
           </div>
         </div>
       </div>
