11540d27839e597631ace8d86573770b2c4e4dd8
Crystal Wang <crystalmaro@gmail.com>
2019-09-02 11:39:43 +0800

    test gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import Header from './Header';
 import Game from './Game';
-// import Button from './Button';
-// import Message from './Message';
 import Progress from './Progress';
-// import Firebase from './firebase';
-import { FirebaseDatabaseProvider } from '@react-firebase/database';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <FirebaseDatabaseProvider>
-        <div>
-          <Header />
-          <Game />
-          {/* <Message /> */}
-          {/* <Button /> */}
-          <Progress />
-          {/* <Firebase /> */}
-        </div>
-      </FirebaseDatabaseProvider>
+      <div>
+        {/* <Header /> */}
+        <Game />
+        {/* <Message /> */}
+        {/* <Button /> */}
+        <Progress />
+        {/* <Firebase /> */}
+      </div>
     );
   }
 }
+
+export default App;
