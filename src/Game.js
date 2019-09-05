c47e482c730c928dff630cf68a726f12e60f921c
Crystal Wang <crystalmaro@gmail.com>
2019-09-05 22:20:53 +0800

    finish initial status bar calculation

--- src/Game.js
+++ src/Game.js
@@ -13,9 +13,13 @@ class Game extends Component {
     yesImg: '../imageStock/yes.png',
     noImg: '../imageStock/no.png',
     data: [],
+    totalRound: [],
+    // randomRound: [],
     round: 0,
-    score: 0
+    score: 0,
+    progressBar: 0,
   };
+
 
   // ============================
   // load firebase data
@@ -27,17 +31,33 @@ class Game extends Component {
       .get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
           let data = [...this.state.data, doc.data()]
-          this.setState({ data });
+          // done: generated a number array for the length of this.state.data
+          // @todo randomly generate numbers without repetition
+          // @todo random yes and no for id
+          let totalRound = [...this.state.totalRound, this.state.data.length]
+          this.setState({
+            data,
+            totalRound
+          });
+          console.log(this.state.totalRound)
+          // console.log(this.state.data.length)
         });
+        // this.setState({ totalRound: this.state.data.length })
       });
   };
+
   componentDidMount() {
     this.loadFirebase();
+    // for (let a = [0, 1], i = a.length; i--;) {
+    //   let random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+    //   console.log(random);
+    // }
   }
   // ============================
   // UI events - click image
   // ============================
   clickImg = e => {
+    console.log(Math.floor((Math.random() * 10) + 1))
     this.setState({
       // overlap both images
       origClass: 'imgBox clicked',
@@ -67,12 +87,15 @@ class Game extends Component {
           break;
       }
     }
+    // progress bar incrementation
+    this.setState((preState) => {
+      return { progressBar: preState.progressBar + (300 / 20) }
+    })
   };
   // ============================
   // UI events - click next
   // ============================
   clickNext = e => {
-    console.log(this.state.data.length - 1)
     if (this.state.isClicked
       && this.state.round < this.state.data.length - 1) {
       this.setState({
@@ -139,8 +162,8 @@ class Game extends Component {
 
   render() {
     let content;
-    if (this.state.data.length >= 20) {
-      // console.log(this.state.data)
+    let dt = this.state.data;
+    if (dt.length >= 20) {
       content =
         <div>
           <div className="gameContainer" id={this.state.round} >
@@ -148,18 +171,18 @@ class Game extends Component {
               style={this.state.yesOnTop == true ? { zIndex: 1 } : null}
               onClick={this.clickImg} className={this.state.origClass}
               id="yes"
-              level={this.state.data[this.state.round].level}
+              level={dt[this.state.round].level}
             >
-              <img src={this.state.data[this.state.round].yes} alt="" />
+              <img src={dt[this.state.round].yes} alt="" />
             </div>
 
             <div
               style={this.state.yesOnTop == false ? { zIndex: 1 } : null}
               onClick={this.clickImg} className={this.state.origClass}
               id="no"
-              level={this.state.data[this.state.round].level}
+              level={dt[this.state.round].level}
             >
-              <img src={this.state.data[this.state.round].no} alt="" />
+              <img src={dt[this.state.round].no} alt="" />
             </div>
           </div>
 
@@ -178,7 +201,7 @@ class Game extends Component {
                 }
               />
             </div>
-            <div className="message">{this.state.data[this.state.round].reason}</div>
+            <div className="message">{dt[this.state.round].reason}</div>
           </div>
 
           <div className="buttonContainer">
@@ -198,16 +221,18 @@ class Game extends Component {
 
           <div className="progressContainer">
             <div className="progressBack">
-              <div className="progressFront"></div>
+              <div className="progressFront"
+                style={this.state.progressBar >= 300 ? { borderRadius: '14px', width: `${this.state.progressBar}px` } : { width: `${this.state.progressBar}px` }}
+              ></div>
               <div className="progressNumber">
-                {this.state.data[this.state.round].level} {this.state.round + 1}/{this.state.data.length}
+                {dt[this.state.round].level} {this.state.round + 1}/{this.state.data.length}
               </div>
             </div>
           </div>
-
         </div>
+
     } else {
-      content = <div>loading</div>
+      content = <div>...</div>
     }
 
     return (
@@ -215,9 +240,6 @@ class Game extends Component {
         <div>current round: {this.state.round + 1}</div>
         <div>current score: {this.state.score}</div>
         {content}
-
-
-
       </div>
     );
   }
