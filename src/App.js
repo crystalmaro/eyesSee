bd252fa15d0788137dec186e47afbd9a87579714
Crystal Wang <crystalmaro@gmail.com>
2019-09-06 18:34:12 +0800

    move firebase data loading from Game.js to App.js

--- src/App.js
+++ src/App.js
@@ -22,12 +22,62 @@ firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
 
+  state = {
+    hello: 'yooo there',
+    data: [], // Fire - complete data loaded from firebase
+    totalRound: [], // Fire - array of number 0 - 19
+    randomRound: [], // Fire - randomized EasyRound, followed by randomize HardRound
+  }
+
+  // ============================
+  // load firebase data
+  // ============================
+  loadFirebase = e => {
+    const db = firebase.firestore();
+    db.collection("imageStock")
+      // .where("level", "==", "easy")
+      .get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          // load and save firebase data into state
+          let data = [...this.state.data, doc.data()]
+          // generate a number array for the length of this.state.data
+          let totalRound = [...this.state.totalRound, this.state.data.length]
+          this.setState({ data, totalRound });
+        });
+        let totalEasyRound = this.state.totalRound.slice(0, 10)
+        let totalHardRound = this.state.totalRound.slice(10)
+        // this.setState({ totalEasyRound, totalHardRound })
+        // randomly generate numbers without repetition - EASY
+        for (let a = totalEasyRound, i = a.length; i--;) {
+          let randomRound = [...this.state.randomRound, a.splice(Math.floor(Math.random() * (i + 1)), 1)[0]];
+          this.setState({ randomRound })
+        }
+        // randomly generate numbers without repetition - HARD
+        for (let a = totalHardRound, i = a.length; i--;) {
+          let randomRound = [...this.state.randomRound, a.splice(Math.floor(Math.random() * (i + 1)), 1)[0]];
+          this.setState({ randomRound })
+        }
+        console.log(this.state.randomRound);
+      });
+  };
+  componentDidMount() {
+    this.loadFirebase();
+  }
+
   render() {
+
     return (
       <FirebaseDatabaseProvider firebase={firebase}>
         <div>
           {/* <Header /> */}
-          <Game />
+          <Game
+            hello={this.state.hello}
+            data={this.state.data}
+            totalRound={this.state.totalRound}
+            randomRound={this.state.randomRound}
+
+
+          />
           {/* <Message /> */}
           {/* <Button /> */}
           {/* <Progress /> */}
--- src/Game.js
+++ src/Game.js
@@ -12,11 +12,13 @@ class Game extends Component {
     isCorrect: true,
     yesImg: '../imageStock/yes.png',
     noImg: '../imageStock/no.png',
-    data: [], // complete data loaded from firebase
-    totalRound: [], // array of number 0 - 19
-    totalEasyRound: [], // totalRound index 0 - 9
-    totalHardRound: [], // totalRound index 10 - 19
-    randomRound: [], // randomized EasyRound, followed by randomize HardRound
+
+    data: [], // Fire - complete data loaded from firebase
+    totalRound: [], // Fire - array of number 0 - 19
+    // totalEasyRound: [], // totalRound index 0 - 9
+    // totalHardRound: [], // totalRound index 10 - 19
+    randomRound: [], // Fire - randomized EasyRound, followed by randomize HardRound
+
     currentRound: 0,
     score: 0,
     progressBar: 0,
@@ -24,41 +26,7 @@ class Game extends Component {
   };
 
 
-  // ============================
-  // load firebase data
-  // ============================
-  loadFirebase = e => {
-    const db = firebase.firestore();
-    db.collection("imageStock")
-      // .where("level", "==", "easy")
-      .get().then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          // load and save firebase data into state
-          let data = [...this.state.data, doc.data()]
-          // generate a number array for the length of this.state.data
-          let totalRound = [...this.state.totalRound, this.state.data.length]
-          this.setState({ data, totalRound });
-        });
-        let totalEasyRound = this.state.totalRound.slice(0, 10)
-        let totalHardRound = this.state.totalRound.slice(10)
-        this.setState({ totalEasyRound, totalHardRound })
-        // randomly generate numbers without repetition - EASY
-        for (let a = this.state.totalEasyRound, i = a.length; i--;) {
-          let randomRound = [...this.state.randomRound, a.splice(Math.floor(Math.random() * (i + 1)), 1)[0]];
-          this.setState({ randomRound })
-        }
-        // randomly generate numbers without repetition - HARD
-        for (let b = this.state.totalHardRound, i = b.length; i--;) {
-          let randomRound = [...this.state.randomRound, b.splice(Math.floor(Math.random() * (i + 1)), 1)[0]];
-          this.setState({ randomRound })
-        }
-        console.log(this.state.randomRound);
-      });
-  };
 
-  componentDidMount() {
-    this.loadFirebase();
-  }
   // ============================
   // UI events - click image
   // ============================
@@ -141,28 +109,28 @@ class Game extends Component {
 
   render() {
     let content;
-    let dt = this.state.data;
+    let data = this.props.data;
+    // let totalRound = this.props.totalRound;
+    let randomRound = this.props.randomRound;
 
-    if (this.state.randomRound.length >= 20) {
-
+    if (randomRound.length >= 20) {
       let yes =
         <div
           style={this.state.yesOnTop == true ? { zIndex: 1 } : null}
           onClick={this.clickImg} className={this.state.origClass}
           id='yes'
-          level={dt[this.state.randomRound[this.state.currentRound]].level}
+          level={data[randomRound[this.state.currentRound]].level}
         >
-          <img src={dt[this.state.randomRound[this.state.currentRound]].yes} alt="" />
+          <img src={data[randomRound[this.state.currentRound]].yes} alt="" />
         </div>;
-
       let no =
         <div
           style={this.state.yesOnTop == false ? { zIndex: 1 } : null}
           onClick={this.clickImg} className={this.state.origClass}
           id='no'
-          level={dt[this.state.randomRound[this.state.currentRound]].level}
+          level={data[randomRound[this.state.currentRound]].level}
         >
-          <img src={dt[this.state.randomRound[this.state.currentRound]].no} alt="" />
+          <img src={data[randomRound[this.state.currentRound]].no} alt="" />
         </div>
 
       let first, second;
@@ -177,7 +145,7 @@ class Game extends Component {
 
       content =
         <div>
-          <div className="gameContainer" id={this.state.randomRound[this.state.currentRound]} >
+          <div className="gameContainer" id={randomRound[this.state.currentRound]}  >
             {first}
             {second}
           </div>
@@ -195,7 +163,7 @@ class Game extends Component {
                 }
               />
             </div>
-            <div className="message">{dt[this.state.randomRound[this.state.currentRound]].reason}</div>
+            <div className="message">{data[randomRound[this.state.currentRound]].reason}</div>
           </div>
 
           <div className="buttonContainer">
@@ -219,7 +187,7 @@ class Game extends Component {
                 style={this.state.progressBar >= 300 ? { borderRadius: '14px', width: `${this.state.progressBar}px` } : { width: `${this.state.progressBar}px` }}
               ></div>
               <div className="progressNumber">
-                {dt[this.state.randomRound[this.state.currentRound]].level} {this.state.currentRound + 1}/{this.state.randomRound.length}
+                {data[randomRound[this.state.currentRound]].level} {this.state.currentRound + 1}/{randomRound.length}
               </div>
             </div>
           </div>
@@ -232,6 +200,7 @@ class Game extends Component {
 
     return (
       <div>
+        <div>{this.props.hello}</div>
         <div>current round: {this.state.currentRound + 1}</div>
         <div>current score: {this.state.score}</div>
         {content}
